5ca8dbac2a160a756ac55b5883d5cf7e72d5d266
Samuel Alfageme Sainz <samuel@alfage.me>
2022-01-10 17:41:26 +0100

    ocproxy: route HEAD requests on the /download endpoint

--- ocproxy/api/api.go
+++ ocproxy/api/api.go
@@ -154,7 +154,7 @@ func (p *proxy) registerRoutes() {
 
 	// public link routes
 	p.router.HandleFunc("/index.php/s/{token}", p.renderPublicLink).Methods("GET", "POST")
-	p.router.HandleFunc("/index.php/s/{token}/download", p.plAuth(p.downloadArchivePL)).Methods("GET")
+	p.router.HandleFunc("/index.php/s/{token}/download", p.plAuth(p.downloadArchivePL)).Methods("GET", "HEAD")
 	p.router.HandleFunc("/index.php/apps/files_sharing/ajax/publicpreview.php", p.tokenAuth(p.getPublicPreview)).Methods("GET")
 
 	// app routes
